8e31919ba5d84b25fb85e42c8a35e44c60ed5d9c
upaliwal1234 <105985373+upaliwal1234@users.noreply.github.com>
2022-06-06 16:31:39 +0530

    Update 3.py

--- Sample_Papers/Paper1_solutions/3.py
+++ Sample_Papers/Paper1_solutions/3.py
@@ -7,8 +7,6 @@ Sample Output
 a=input()
 l=list(a)
 e=list(set(l))
-c=[i for i in l if i in e]
-print(c)
 d={}
-[d.update({c[i]:l.count(c[i])}) for i in range(len(c))]    
+[d.update({l[i]:l.count(l[i])}) for i in range(len(l))]    
 print(d)
