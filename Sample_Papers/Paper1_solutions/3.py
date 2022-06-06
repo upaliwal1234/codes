79f1fc28db677e49709d2a944f706d915ac97238
upaliwal1234 <105985373+upaliwal1234@users.noreply.github.com>
2022-06-06 16:22:34 +0530

    Create 3.py

---
+++ Sample_Papers/Paper1_solutions/3.py
@@ -0,0 +1,15 @@
+'''Write a Python Program to find the frequency of each character in string using dictionary comprehension.
+Sample input
+banana
+Sample Output
+{'b': 1, 'a': 3, 'n': 2}
+'''
+a=input()
+l=list(a)
+e=list(set(l))
+c=[i for i in l if i in e]
+print(c)
+d={}
+for i in range(len(c)):
+    d.update({c[i]:l.count(c[i])})
+print(d)
